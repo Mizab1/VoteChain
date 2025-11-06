5d7c893e6905c4cf84c2396edba55e8efaf6eed5
Mizab1 <mizabmansari@gmail.com>
2025-11-07 01:22:58 +0530

    Removed redundant files

--- index.js
+++ index.js
@@ -35,14 +35,6 @@ app.get('/js/login.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/js/login.js'))
 });
 
-app.get('/js/simple-fixes.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/js/simple-fixes.js'))
-});
-
-app.get('/js/simple-metamask.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/js/simple-metamask.js'))
-});
-
 app.get('/css/login.css', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/css/login.css'))
 });
@@ -94,56 +86,6 @@ app.get('/dist/voter.bundle.js', (req, res) => {
 // Serve the favicon.ico file
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/favicon.ico'));
-});
-
-// Serve our consolidated pages without authentication
-app.get('/admin-consolidated.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/admin-consolidated.html'));
-});
-
-app.get('/voter-consolidated.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/voter-consolidated.html'));
-});
-
-// Serve other HTML files for testing
-app.get('/metamask-minimal.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/metamask-minimal.html'));
-});
-
-app.get('/metamask-basic.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/metamask-basic.html'));
-});
-
-app.get('/connect-metamask.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/connect-metamask.html'));
-});
-
-app.get('/metamask-troubleshoot.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/metamask-troubleshoot.html'));
-});
-
-app.get('/admin-direct-connect.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/admin-direct-connect.html'));
-});
-
-app.get('/admin-minimal.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/admin-minimal.html'));
-});
-
-app.get('/admin-simple-working.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/admin-simple-working.html'));
-});
-
-app.get('/voter-simple.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/voter-simple.html'));
-});
-
-app.get('/admin-simple-storage.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/admin-simple-storage.html'));
-});
-
-app.get('/voter-simple-storage.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/html/voter-simple-storage.html'));
 });
 
 // Start the server
